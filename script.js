9e360ed93c5f3a18e9f76522cc0b0574eae6da7b
alez05 <cercel.alexandru436@gmail.com>
2024-01-22 13:26:51 +0000

    implement script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const displayScores = () => {
+  fetch('./data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const scoresList = document.getElementById('scores');
+
+      data.forEach((scores) => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('scores__item');
+
+        const icon = document.createElement('img');
+        icon.src = scores.icon;
+        icon.classList.add('scores__icon');
+
+        const text = document.createElement('span');
+        text.textContent = `${scores.category}`;
+        text.classList.add('scores__text');
+
+        const sp = document.createElement('span');
+        sp.textContent = `${scores.score}/${100}`;
+        sp.classList.add('scores__sp');
+
+        listItem.appendChild(icon);
+        listItem.appendChild(text);
+        listItem.appendChild(sp);
+
+        scoresList.appendChild(listItem);
+      });
+    })
+
+    .catch((error) => console.error(error));
+};
+
+displayScores();
+
+// learned how to manipulate the DOM with JS and how to use fetch to get data from a JSON file.
